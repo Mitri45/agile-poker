8a739fe8400829f1d77b6a174f79e34efd1530cc
Dmitrii Bludov <dmitrii.website@gmail.com>
2024-01-18 07:49:33 +1300

    Wording

--- app/src/Pages/StartPage.tsx
+++ app/src/Pages/StartPage.tsx
@@ -85,7 +85,7 @@ export default function StartPage() {
               autoComplete="off"
               required
               className=" border-0 w-full bg-white/5 px-3.5 py-2  shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-              placeholder="What topic we are discussing?"
+              placeholder="What topic are we discussing? "
             />
             {errors.roomName && <span>This field is required</span>}
           </div>
